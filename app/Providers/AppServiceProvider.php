76c2504f30e3ac7e60706eaa5a964550d9bd3898
salperens <alperen@alperensarikaya.com>
2025-11-22 18:19:51 +0300

    move match simulatiom binds to new service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,6 @@
 
 namespace App\Providers;
 
-use App\MatchSimulation\Contracts\GoalGenerator;
-use App\MatchSimulation\Contracts\MatchSimulator;
-use App\MatchSimulation\Contracts\OutcomeCalculator;
-use App\MatchSimulation\Contracts\RandomGenerator;
-use App\MatchSimulation\DefaultMatchSimulator;
-use App\MatchSimulation\Goals\SimpleGoalGenerator;
-use App\MatchSimulation\Outcome\PowerBasedOutcomeCalculator;
-use App\MatchSimulation\Random\NativeRandomGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,10 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(RandomGenerator::class, NativeRandomGenerator::class);
-        $this->app->bind(OutcomeCalculator::class, PowerBasedOutcomeCalculator::class);
-        $this->app->bind(GoalGenerator::class, SimpleGoalGenerator::class);
-        $this->app->bind(MatchSimulator::class, DefaultMatchSimulator::class);
+        //
     }
 
     /**
---
+++ app/Providers/MatchSimulationServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\MatchSimulation\Contracts\GoalGenerator;
+use App\MatchSimulation\Contracts\MatchSimulator;
+use App\MatchSimulation\Contracts\OutcomeCalculator;
+use App\MatchSimulation\Contracts\RandomGenerator;
+use App\MatchSimulation\DefaultMatchSimulator;
+use App\MatchSimulation\Goals\SimpleGoalGenerator;
+use App\MatchSimulation\Outcome\PowerBasedOutcomeCalculator;
+use App\MatchSimulation\Random\NativeRandomGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class MatchSimulationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(RandomGenerator::class, NativeRandomGenerator::class);
+        $this->app->bind(OutcomeCalculator::class, PowerBasedOutcomeCalculator::class);
+        $this->app->bind(GoalGenerator::class, SimpleGoalGenerator::class);
+        $this->app->bind(MatchSimulator::class, DefaultMatchSimulator::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
